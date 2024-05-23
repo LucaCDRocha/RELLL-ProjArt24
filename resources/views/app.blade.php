b8cf49fda90d3ecee3e595aaa06a3458d3d9cd15
LucaCDRocha <lucadrocha@gmail.com>
2024-05-23 12:58:37 +0200

    Update dark mode styles for components

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'RELLL') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+
+    <script>
+        JSON.parse(localStorage.getItem('darkMode')) ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+    </script>
+</head>
+
+<body class="font-sans antialiased dark:bg-gray-800 dark:text-white">
+    @inertia
+</body>
+
 </html>
