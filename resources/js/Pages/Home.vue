8304168e684c9f2c5442509c40efd01732c1f63c
LucaCDRocha <lucadrocha@gmail.com>
2024-05-23 10:53:48 +0200

    Change front from blade to vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Home" />
+    
+
+    <h1 class="text-xl font-semibold text-black dark:text-white">Home</h1>
+</template>
+
+<style scoped></style>
--- resources/js/appHome.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import App from './AppHome.vue';
-
-const myApp = createApp(App)
-myApp.mount('#app');
