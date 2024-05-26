514c2b4d45b9e4793734423e0acc760183acb7b7
LucaCDRocha <lucadrocha@gmail.com>
2024-05-26 13:42:30 +0200

    Update button styles for PrimaryButton and SecondaryButton components

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
     <button
         :type="type"
-        class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-full font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"
+        class="inline-flex items-center px-4 py-2 bg-green-50 dark:bg-green-950 border border-green-800 dark:border-green-200 rounded-full font-semibold text-sm text-green-800 dark:text-green-200 shadow-sm hover:bg-green-100 dark:hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 dark:focus:ring-offset-green-800 disabled:opacity-25 transition ease-in-out duration-150"
     >
         <slot />
     </button>
