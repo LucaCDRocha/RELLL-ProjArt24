9f20a9d40fcca0ceb6c7ad4b8cec2272ba6a9146
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-05-27 13:37:23 +0200

    Seeder ranking, comments et likes

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Helpers\JsonHelper;
 
 class CommentSeeder extends Seeder
 {
@@ -12,6 +14,24 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        try {
+            $data = JsonHelper::readJson('/interactionsTest.json'); //récupère des infos dans le JSON
+            $comments = collect($data['comments']);
+
+            foreach ($comments as $comment) {
+                $trailId = JsonHelper::getInfoByValue('trails', 'name', $comment['trail']);
+                $userId = JsonHelper::getInfoByValue('users', 'name', $comment['user']);
+
+                Comment::create([
+                    'text' => $comment['content'],
+                    'trail_id' => $trailId,
+                    'user_id' => $userId,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+            }
+        } catch (\Exception $e) {
+            $this->command->error($e->getMessage());
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,22 +15,42 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'email_verified_at' => now(),
-            'password' => "12345Test",
-            'is_admin' => true
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => "12345Test",
+        //     'is_admin' => true
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'Marc',
+        //     'email' => 'marc@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => "12345Bob",
+        //     'is_admin' => true
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'Sam',
+        //     'email' => 'sam@example.com',
+        //     'email_verified_at' => now(),
+        //     'password' => "yes1234",
+        //     'is_admin' => true
+        // ]);
+        
 
-        // création des parcours tests
-        $this->call(TagSeeder::class);
-        $this->call(LocationSeeder::class);
-        $this->call(ThemesSeeder::class);
-        $this->call(InterestPointSeeder::class);
-        $this->call(ImgSeeder::class);
-        $this->call(TrailsSeeder::class);
-        $this->call(InterestPointTrailSeeder::class);
-        $this->call(ThemeTrailSeeder::class);
+        // // création des parcours tests
+        // $this->call(TagSeeder::class);
+        // $this->call(LocationSeeder::class);
+        // $this->call(ThemesSeeder::class);
+        // $this->call(InterestPointSeeder::class);
+        // $this->call(ImgSeeder::class);
+        // $this->call(TrailsSeeder::class);
+        // $this->call(InterestPointTrailSeeder::class);
+        // $this->call(ThemeTrailSeeder::class);
+
+        // // création de commentaires et de notes test
+        // $this->call(CommentSeeder::class);
+        // $this->call(LikeSeeder::class);
+        // $this->call(RankingSeeder::class);
     }
 }
