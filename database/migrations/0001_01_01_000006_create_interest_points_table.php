c960017ae8f1c9d98368800ce85f4e7ba398c7d6
LucaCDRocha <lucadrocha@gmail.com>
2024-05-30 11:38:43 +0200

    Add uuid in migrations for identify some tables

--- database/migrations/0001_01_01_000006_create_interest_points_table.php
+++ database/migrations/0001_01_01_000006_create_interest_points_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('interest_points', function (Blueprint $table) {
             $table->increments('id');
+            $table->uuid('uuid')->unique()->default(DB::raw('(UUID())'));
             $table->string('name');
             $table->text('description');
             $table->text('url');
