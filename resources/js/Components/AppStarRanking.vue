da15418529f1933eaac31f50f74f809998eea9da
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-06-04 11:54:17 +0200

    Création étoiles et accordéon et modifications boutons

---
+++ resources/js/Components/AppStarRanking.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    props: {
+        rating: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        integerRating() {
+            return Math.floor(this.rating);
+        },
+        decimalRating() {
+            return this.rating - Math.floor(this.rating);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex items-center space-x-1">
+        <template v-for="index in 5">
+            <span v-if="index <= integerRating" class="material-symbols-rounded full-star">star</span>
+            <span v-else-if="index === integerRating + 1 && decimalRating >= 0.5" class="material-symbols-rounded">
+                star_half
+            </span>
+            <span v-else class="material-symbols-rounded">star</span>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+span {
+    font-size: 1rem;
+}
+
+.full-star {
+    font-variation-settings: 'FILL' 1;
+}
+
+</style>
